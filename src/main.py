99d340e1d3bf729640abcb91ce697d1b4c54f49b
NicKarlen <nicolas.k@gmx.net>
2022-10-08 13:32:57 +0200

    add: main.py & functions.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import functions as f
+
+def step_1():
+    """
+        Get the raw data from MagicEden via API
+    """
+    f.get_activities(collection="degods")
+
+
+
+
+if __name__ == "__main__":
+    step_1()
