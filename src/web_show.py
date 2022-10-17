badfe1e9ffee6f3d783de5c0d64ef438a385732b
NicKarlen <nicolas.k@gmx.net>
2022-10-17 12:17:52 +0200

    Update web_show.py

--- src/web_show.py
+++ src/web_show.py
@@ -10,9 +10,9 @@ import functions as f
 x = st.slider('Select a value', min_value=5,max_value=50)
 
 df = f.read_df_from_sql(table_name="degods")
-df["PercChanges"] = df["cFP in Dollar"].pct_change() * 100
-df["MA"] = df["PercChanges"].rolling(window=x).mean()
+#df["PercChanges"] = df["cFP in Dollar"].pct_change() * 100
+df["MA"] = df["cFP in Dollar"].rolling(window=x).mean()
 
-st.dataframe(df[["ts","cFP in Dollar", "PercChanges", "MA"]])
+st.dataframe(df[["ts","cFP in Dollar", "MA"]])
 
 st.line_chart(data=df, x="ts",y="MA")
